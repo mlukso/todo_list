62ea26b3e9c9614dc86a6e05df7aa6a92f6aabc4
Matt Lukso <mk.lukso@gmail.com>
2018-08-30 00:17:10 -0500

    working but needs a delete option still

--- src/App.js
+++ src/App.js
@@ -1,18 +1,41 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+      this.state = {
+        inputValue: '',
+        list: [],
+      };
+  }
+  // user has to add items to the list
+  handleInputChange=(e) => {
+    this.setState({inputValue: e.target.value})
+  };
+
+  handleInputSubmit=(e) => {
+    e.preventDefault();
+    const newList = this.state.list;
+    newList.push(this.state.inputValue);
+    this.setState({list: newList, inputValue: ''});
+  };
+
+  renderList=() => {
+    return this.state.list.map((item, index) => {
+      return <div><p key={index}>{item}<button>X</button></p></div>
+    })
+  };
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Does this work?</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <form>
+        <h2>My Todo List</h2>
+        <input value={this.state.inputValue} onChange={this.handleInputChange} type = "text"></input>
+        <button onClick={this.handleInputSubmit}>Add Todo</button>
+        </form>
+        {this.renderList()}
       </div>
     );
   }
