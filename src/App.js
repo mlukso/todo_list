0fd2626b352e8c0cb8130ef3e87184cf693ef343
Matt Lukso <mk.lukso@gmail.com>
2018-09-01 17:42:16 -0500

    can delete/add items

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
 import './App.css';
 
+const stylingObject  = {
+  color: 'blue'
+}
+
+const strikeThrough  = {
+  textDecorationLine: 'line-through'
+}
+
 class App extends Component {
   constructor(props) {
     super(props);
       this.state = {
         inputValue: '',
         list: [],
+        done: false,
       };
   }
   // user has to add items to the list
@@ -17,23 +26,67 @@ class App extends Component {
   handleInputSubmit=(e) => {
     e.preventDefault();
     const newList = this.state.list;
+    if(this.state.inputValue){
     newList.push(this.state.inputValue);
     this.setState({list: newList, inputValue: ''});
+    } 
   };
 
-  renderList=() => {
-    return this.state.list.map((item, index) => {
-      return <div><p key={index}>{item}<button>X</button></p></div>
+  handleRemoveItem=(item, index) => {
+    const removeItem = this.state.list.slice();
+    removeItem.splice(index, 1)
+    this.setState({
+      list: removeItem
     })
+  };
+
+  toggleTodoDone=(item, index) =>{
+    if (this.state.done === false){
+      this.setState({
+        done: true,
+      }) 
+    } else {
+      this.setState({
+        done: false
+      }) 
+    }
   }
 
+  // isDone(e){
+  //   e.target.value = 'new'
+  // }
+
+  renderButton=() => {
+    if(this.state.inputValue){
+      return <button onClick={this.handleInputSubmit}>Add ToDo</button>
+    }
+  };
+
+  // renderList=() => {
+  //   return this.state.list.map((item, id) => {
+  //     const listItems = `${item}`
+  //     return <ul><li key={id}>{listItems}<button>X</button></li></ul>
+  //   })
+  // };
+
+  renderList = () => {
+    return this.state.list.map((item, index) => 
+      <ul>
+        <li key={index} style={this.props.className}>
+          {item} 
+          <button onClick={()=>{this.toggleTodoDone(item, index)}}>✓</button>
+          <button onClick={()=>{this.handleRemoveItem(item, index)}}>X</button>
+        </li>
+      </ul>)
+  }
+  
   render() {
     return (
       <div>
         <form>
-        <h2>My Todo List</h2>
-        <input value={this.state.inputValue} onChange={this.handleInputChange} type = "text"></input>
-        <button onClick={this.handleInputSubmit}>Add Todo</button>
+        <h2 style={stylingObject}>My ToDo List</h2>
+        <input placeholder='type something...' value={this.state.inputValue} onChange={this.handleInputChange} type = "text"></input>
+        {this.renderButton()}
         </form>
         {this.renderList()}
       </div>
